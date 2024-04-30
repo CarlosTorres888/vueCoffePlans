c033f877d8d0760aeb3a57521470c2d3d9932779
Carlos Torres <torresmeneses888@gmail.com>
2024-04-30 12:54:53 -0600

    ⭐Slots

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,11 @@
 <script setup>
-import planPicker from './components/plan-picker.vue';
-import appTitle from './components/app-title.vue';
-import appSubtitle from './components/app-subtitle.vue';
-import {ref} from 'vue';
-const showPlans = ref(true);
+import FancyButton from './components/FancyButton.vue'
 </script>
 
 <template>
-  <header>
-    <div>
-      <span id="logo">Liquid Gold Box</span>
-      <img src="./assets/logo.png" alt="logo">
-    </div>
-  </header>
-
-  <div class="content">
-    <app-title title="Vacaciones"/>
-    <app-subtitle subtitle="Viajamos por el mundo para encontrar el mejor café de origen único para ti"/>
-    <label>
-      <input type="checkbox" v-model="showPlans">
-      Mostrar Selector de Planes
-    </label>
-    <plan-picker :plans="['el soltero','El colombiano','Extra']" v-if="showPlans"/>
-  </div>
+ <FancyButton/>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
